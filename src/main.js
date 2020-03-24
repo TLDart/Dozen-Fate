23f5958bc991757fae3a34d265db28d7147d7e0b
MikeLrUC <miguelrabuge12@gmail.com>
2020-03-24 20:04:03 +0000

    Fixed main.js

--- src/main.js
+++ src/main.js
@@ -1,25 +1,13 @@
-class MenuScene extends Phaser.Scene {
-    constructor(){
-        super("MenuScene");
-        this.background = null;
-        this.banner = null;
-    }
 
-    preload(){
-        this.load.image("banner","assets/Logo/logo.png");
-        this.load.image("background","assets/Sprites/Others/Background.png")
-    }
-    create(){
-        this.background = this.add.tileSprite(0,0,500,600,"background").setOrigin(0,0);
-        this.banner = this.add.sprite(0,0,"banner");
-        this.banner.scaleX = 0.5;
-        this.banner.scaleY = 0.5;
-        this.banner.setPosition(250,200);
+(function () {
+    window.onload = main
+})();
 
-
-    }
-    update(){
-        this.background.tilePositionY += 0.2;
-    }
-
+function main() {
+    var game = new Phaser.Game({
+        type: Phaser.CANVAS,
+        width: 500,
+        height: 600,
+        scene: [IntroScene]
+    });
 }
